c03624695bae11e03c9e38765ebaf7fe33cac7c5
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2023-06-11 13:57:07 +0700

    yayoye

---
+++ Daftar Inventaris Barang Milik Negera/src/mencoba.java
@@ -0,0 +1,6 @@
+public class mencoba{
+    public static void main(String[] args) {
+        System.out.println("PLEEKKK");
+        System.out.println("INPO PLEK");
+    }
+}
--- Daftar Inventaris Barang Milik Negera/src/papope.java
+++ Daftar Inventaris Barang Milik Negera/src/papope.java
@@ -1,5 +1,5 @@
 public class papope {
     public static void main(String[] args) {
-        
+        System.out.println("apeee");
     }
 }
