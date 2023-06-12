2bcc93b7dc59a7ebb0c47200171bbe27dd2087a0
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2023-06-12 08:04:28 +0700

    papope

--- Daftar Inventaris Barang Milik Negera/src/papope.java
+++ Daftar Inventaris Barang Milik Negera/src/papope.java
@@ -2,5 +2,8 @@ public class papope {
     public static void main(String[] args) {
         System.out.println("apeee");
         System.out.println("YAYOYEYEYEYE");
+        for(int i=0; i<15;i++){
+            System.out.println("GAPLEK KING!");
+        }
     }
 }
