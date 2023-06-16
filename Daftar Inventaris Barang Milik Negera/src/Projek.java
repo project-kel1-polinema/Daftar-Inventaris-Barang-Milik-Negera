83fab879fa0f613872982a7150092d0422260223
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2023-06-16 19:42:32 +0700

    UDAH

--- Daftar Inventaris Barang Milik Negera/src/Projek.java
+++ Daftar Inventaris Barang Milik Negera/src/Projek.java
@@ -293,6 +293,7 @@ public class Projek {
         }
     }
 
+    // MEMBUAT FILE .txt BUAT EXPORT
     public static void writeArrayToFile(Projek[] pj, String fileName) {
         String garis = "+====================================================================+\n";
         String atas = "| Kode BMN    | Tahun Anggaran  | Nama Barang\t\t| Nilai(Rp.) |\n";
